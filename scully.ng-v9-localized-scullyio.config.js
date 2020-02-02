eed1ac5b014c9ca715ad9ffd1377729588006128
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2020-02-02 12:04:20 +0100

    updated

--- scully.ng-v9-localized-scullyio.config.js
+++ scully.ng-v9-localized-scullyio.config.js
@@ -1,10 +1,10 @@
 const {RouteTypes} = require('@scullyio/scully');
 const {MinifyHtml} = require('scully-minify-html');
 const {DisableAngular} = require('scully-disable-angular');
-const {Test} = require('./plugins/testPlugin');
 
 // custom plugins in ./plugins/*.js
 const {News} = require('./plugins/newsPlugin');
+const {Test} = require('./plugins/testPlugin');
 
 const postRenderers = [MinifyHtml, DisableAngular, Test];
 
