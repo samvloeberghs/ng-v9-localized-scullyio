2b610f3ea1bf1cc210f0bd4a5a3ca13ba2873326
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2020-02-04 22:58:43 +0100

    removed hammerjs

--- scully.ng-v9-localized-scullyio.config.js
+++ scully.ng-v9-localized-scullyio.config.js
@@ -1,33 +1,15 @@
-const {RouteTypes} = require('@scullyio/scully');
 const {MinifyHtml} = require('scully-minify-html');
 const {DisableAngular} = require('scully-disable-angular');
 
-// custom plugins in ./plugins/*.js
-const {News} = require('./plugins/newsPlugin');
-const {Test} = require('./plugins/testPlugin');
-
-const postRenderers = [DisableAngular, MinifyHtml, Test];
-
-const minifyHtmlOptions = {
-  removeComments: false,
-};
+const postRenderers = [DisableAngular, MinifyHtml];
 
 exports.config = {
   projectRoot: './src',
   defaultPostRenderers: postRenderers,
-  minifyHtmlOptions,
   routes: {
     '/news/:id/:slug': {
       type: News,
       url: 'http://localhost:4321/assets/news.json'
-    },
-    '/blog/:slug': {
-      type: RouteTypes.contentFolder,
-      slug: {
-        folder: './blog'
-      },
-      postRenderers: postRenderers,
-      minifyHtmlOptions
     }
   }
 };
